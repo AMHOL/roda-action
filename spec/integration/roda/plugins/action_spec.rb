28eb16b64de33007e5106c251c9975f2c8a3f184
Andy Holland <andyholland1991@aol.com>
2015-05-14 21:27:08 +0100

    Add spec for bind_arguements

--- spec/integration/roda/plugins/action_spec.rb
+++ spec/integration/roda/plugins/action_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
-module Test
-  class Controller
-    def index
-      'It worked!!!'
-    end
+class Controller
+  def index
+    'It worked!!!'
+  end
+
+  def show(id)
+    "Now showing #{id}"
   end
 end
 
-Roda.register(:controller) { Test::Controller.new }
+Roda.register(:controller) { Controller.new }
 Roda.route do |r|
   r.root(&Roda.action(:controller, :index))
+
+  r.on :id do |id|
+    r.get(&Roda.action(:controller, :show).bind_arguments(id))
+  end
 end
 
 RSpec.describe 'action plugin' do
@@ -18,5 +24,11 @@ RSpec.describe 'action plugin' do
     get '/', {}
 
     expect(last_response.body).to eq('It worked!!!')
+  end
+
+  it 'allows binding of arguments to action methods' do
+    get '/1337', {}
+
+    expect(last_response.body).to eq('Now showing 1337')
   end
 end
